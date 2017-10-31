572e57552163ba73d3e35525372d3d3af688aac4
Alex DeCamillo <alexdecamillo@yahoo.com>
2017-10-31 16:36:03 -0400

    Finished the mount/unmount modules and the showsuper module. Fixed the input argument parser.

--- main.c
+++ main.c
@@ -6,75 +6,83 @@
 #include "minix.h"
 #include "modules.c"
 
-int fd;
+int *fd;
 
-void runCommand(char c[]);
+void runCommand(char *c[], int len);
 
 int main(int argc, char *argv[]){
 
-    int i;
+    int commandLength;
+    fd = malloc(sizeof(int));
     char prompt[] = "Minix Console$ ";
     char *input = malloc(50), *token, *delim=" ", *buffer;
 
     // initialize the command pointers
     char **command = (char**)malloc(2*sizeof(char*));
-    for(i = 0; i < 2; i++)
+    for(int i = 0; i < 2; i++)
         command[i] = (char *)malloc(50*sizeof(char *));
 
     // the main loop for the minix console
     while (1) {
         printf("%s", prompt);
-        scanf("%s", input);
+        fgets(input, 50, stdin);
 
         buffer = strdup(input);
 
+        commandLength = 0;
         while ( (token = strsep(&buffer, delim)) != NULL ) {
-            command[i] = token;
-            //printf("%s\n", command[i]);
+            if (token[strlen(token)-1] == '\n')
+                token[strlen(token)-1] = '\0';
+            command[commandLength] = token;
+            commandLength++;
         }
-        runCommand(input);
+        runCommand(command, commandLength);
     }
 
-    /*
-    int fd = open("./imagefile.img", O_RDWR);
-    char *buffer = malloc(sizeof(struct minix_super_block));
-    size_t bytesToRead = 1024;
-    ssize_t pos = 0;
-    SUPER_BLOCK *sb = malloc(sizeof(SUPER_BLOCK));
-
-    if(lseek(fd, 1024, SEEK_SET) < 0) return 1;
-
-    pos = read(fd, sb, sizeof(SUPER_BLOCK));
-    printf("%ld\n", pos);
-    */
 }
 
-// need to get the argument parser working better before this works fully
-void runCommand(char c[]){
+// takes the command array and the length of the array and calls the
+//  appropiate module from modules.c
+void runCommand(char *c[], int len){
 
-    if (strcmp(c, "quit") == 0){
+    if (strcmp(c[0], "quit") == 0) {
         exit(1);
-    } else if(strcmp(c, "help")) {
-        // display help information
-    } else if(strcmp(c, "minimount")) {
-        // runs open(filename, O_RDWR) and returns the file descriptor
-    } else if(strcmp(c, "miniumount")) {
-        // run close(fd) to close the file descriptor
+    } else if(strcmp(c[0], "help") == 0) {
+        helpInfo();
+        return;
+
+    } else if(strcmp(c[0], "minimount") == 0) {
+        if(len >= 2)
+            minimount(fd, c[1]);
+        else {
+            char *error = "ERROR: You must specify an image to mount\n";
+            write(2, error, strlen(error));
+            free(error);
+        }
+        return;
+
+    } else if(strcmp(c[0], "miniumount") == 0) {
+        miniumount(*fd);
+        return;
+
+    } else if(strcmp(c[0], "showsuper") == 0) {
+        if (*fd < 3) { noMount(); return; }
+        superblock(*fd);
+
+    } else if(strcmp(c[0], "traverse") == 0) {
+        if (*fd < 3) { noMount(); return; }
+
+    } else if(strcmp(c[0], "showzone") == 0) {
+        if (*fd < 3) { noMount(); return; }
+
+    } else if(strcmp(c[0], "showfile") == 0) {
+        if (*fd < 3) { noMount(); return; }
+
     } else {
-        // check if we have a valid file descriptor to use and send
-        // and error back if not
-    }
-
-    if(strcmp(c, "showsuper")) {
-        // read in the block data starting at block 1024 and format/display the
-        // appropiate info
-    } else if(strcmp(c, "traverse")) {
-
-    } else if(strcmp(c, "showzone")) {
-
-    } else if(strcmp(c, "showfile")) {
-
-    } else {
-        // send an error for not passing a vaild command
+        char *error = malloc(100);
+        strcat(error, c[0]);
+        strcat(error, ": command not found. Use the command 'help' for a list of valid commands\n");
+        write(2, error, strlen(error));
+        free(error);
     }
 }
--- modules.c
+++ modules.c
@@ -1,3 +1,80 @@
-int minimount(char *filename){
-    return open(filename, O_RDWR);
+// tries to open filename and assign the file descriptor to fd
+void minimount(int *fd, char *filename) {
+    // if the file descriptor is at least 3, it has been assigned already
+    if (*fd >= 3) {
+        char *error = "ERROR: Please unmount the current image before mounting a new one\n";
+        write(2, error, strlen(error));
+        free(error);
+    } else {
+        *fd = open(filename, O_RDWR);
+        if (*fd < 3) {
+            char *error = malloc(100);
+            strcat(error, "ERROR: No valid file '");
+            strcat(error, filename);
+            strcat(error, "' for argument IMAGE_NAME in command 'minimount'\n");
+            write(2, error, strlen(error));
+            free(error);
+        } else {
+            char *response = "Image sucessfully mounted\n";
+            write(0, response, strlen(response));
+            free(response);
+        }
+    }
+}
+
+// closes the passed file descriptor if it is opened
+void miniumount(int fd) {
+    // if the file descriptor isn't at least 3, there's nothing to unmount
+    if (fd >= 3) {
+        int umount = close(fd);
+        if (umount == 0) {
+            fd = -1;
+            char *response = "Image sucessfully unmounted\n";
+            write(0, response, strlen(response));
+            free(response);
+        } else {
+            char *error = "ERROR: There was a problem unmounting the image\n";
+            write(2, error, strlen(error));
+            free(error);
+        }
+    } else {
+        char *error = "ERROR: There is no mounted image to unmount\n";
+        write(2, error, strlen(error));
+        free(error);
+    }
+}
+
+// prints an error if commands that require a mounted image are called without a
+//  mounted image
+void noMount() {
+    char *error = "ERROR: You must mount a valid image file before using this command\n";
+    write(2, error, strlen(error));
+    free(error);
+}
+
+// prints the contents of the super block
+void superblock(int fd) {
+    // try to move the offset to the beginning of the super block
+    if(lseek(fd, 1024, SEEK_SET) < 0) {
+        char *error = "ERROR: There was an problem retrieving the super block\n";
+        write(2, error, strlen(error));
+        free(error);
+    } else {
+        struct minix_super_block *sb = malloc(sizeof(SUPER_BLOCK));
+        ssize_t pos = read(fd, sb, sizeof(SUPER_BLOCK));
+        printf("%s%d\n", "number of inodes:       ", sb->s_ninodes);
+        printf("%s%d\n", "number of zones:        ", sb->s_nzones);
+        printf("%s%d\n", "number of imap_blocks:  ", sb->s_imap_blocks);
+        printf("%s%d\n", "number of zmap_blocks:  ", sb->s_zmap_blocks);
+        printf("%s%d\n", "first data zone:        ", sb->s_firstdatazone);
+        printf("%s%d\n", "log zone size: 	        ", sb->s_log_zone_size);
+        printf("%s%d\n", "max size: 	        ", sb->s_max_size);
+        printf("%s%d\n", "magic: 	                ", sb->s_magic);
+        printf("%s%d\n", "state:                  ", sb->s_state);
+        printf("%s%d\n", "zones:                  ", sb->s_zones);
+    }
+}
+
+void helpInfo() {
+    // print the information regarding possible commands
 }
